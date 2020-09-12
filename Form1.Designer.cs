8f060e191060c72459ae4cf4138cba2ec1708342
amores <dell@AMORES-PC>
2020-09-13 05:01:10 +0800

    c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Amores_Coin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 332);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(298, 71);
+            this.trackBar1.Maximum = 300;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(286, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Threshold";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Count Value";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 418);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Find Value of Coins";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using ImageProcess2;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Reflection;
+
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+namespace Amores_Coin
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded;
+        Bitmap processed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loaded = new Bitmap(@"C:\Users\dell\Downloads\coins1.jpg");
+            BitmapFilter.Scale(ref loaded, ref processed, 251, 332);
+            loaded = processed;
+            pictureBox1.Image = loaded;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            BitmapFilter.NegativeThreshold(ref loaded, ref processed, trackBar1.Value);
+            pictureBox1.Image = processed;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double sum = 0;
+            //locate objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.ProcessImage(processed);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            Graphics g = Graphics.FromImage(processed);
+            Pen redPen = new Pen(Color.Red, 2);
+
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            Console.WriteLine(blobs.Length);
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                AForge.Point center;
+                float radius;
+
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(redPen,
+                        (int)(center.X - radius),
+                        (int)(center.Y - radius),
+                        (int)(radius * 2),
+                        (int)(radius * 2));
+
+                    if (blobs[i].Area >= 240 && blobs[i].Area <= 290)
+                    {
+                        sum += 0.05;
+                    }
+                    if (blobs[i].Area >= 315 && blobs[i].Area <= 360)
+                    {
+                        sum += 0.1;
+                    }
+                    if (blobs[i].Area >= 420 && blobs[i].Area <= 500)
+                    {
+                        sum += 0.25;
+                    }
+                    if (blobs[i].Area >= 610 && blobs[i].Area <= 680)
+                    {
+                        sum += 1;
+                    }
+                    if (blobs[i].Area >= 760 && blobs[i].Area <= 840)
+                    {
+                        sum += 5;
+                    }
+                }
+
+            }
+
+
+            redPen.Dispose();
+            g.Dispose();
+
+            
+            MessageBox.Show("Total: " + sum);
+        }
+    }
+}
